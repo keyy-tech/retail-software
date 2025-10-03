cdd3fa7347f0c8271ea9664a521895c0abaff894
Kotoka Emmanuel <emmanuelyayrakotoka@gmail.com>
2025-10-03 22:15:51 +0000

    Work done

--- sales/admin.py
+++ sales/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from sales.models import Sales, SaleItems
+
+
 # Register your models here.
+@admin.register(Sales)
+class SalesAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(SaleItems)
+class SaleItemsAdmin(admin.ModelAdmin):
+    pass
